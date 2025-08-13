ae77665b5b78a09e79f331118e761678d3f94e54
inch307 <39515837+inch307@users.noreply.github.com>
2025-08-13 13:57:47 +0900

    Add files via upload

--- transform_pm.ps1
+++ transform_pm.ps1
@@ -6,18 +6,6 @@ $eps_values = @(1.0, 2.0, 3.0, 4.0, 5.0)
 foreach ($t in $t_values) {
     # 각 eps 값에 대하여 반복
     foreach ($eps in $eps_values) {
-        $command = "python pm_transform_train_test_batch_label.py --t $t --eps $eps --transform_label_numerical False --transform_label_categorical True --transform_label_log False --csv_path data/credit.csv --label_col label"
-
-        Write-Host "Executing: $command" -ForegroundColor Green
-
-        Invoke-Expression $command
-    }
-}
-
-# 각 t 값에 대하여 반복
-foreach ($t in $t_values) {
-    # 각 eps 값에 대하여 반복
-    foreach ($eps in $eps_values) {
         $command = "python pm_transform_train_test_batch_label.py --t $t --eps $eps --transform_label_numerical True --transform_label_categorical False --transform_label_log True --csv_path data/OnlineNewsPopularity.csv --label_col shares"
 
         Write-Host "Executing: $command" -ForegroundColor Green
@@ -42,6 +30,18 @@ foreach ($t in $t_values) {
     # 각 eps 값에 대하여 반복
     foreach ($eps in $eps_values) {
         $command = "python pm_transform_train_test_batch_label.py --t $t --eps $eps --transform_label_numerical False --transform_label_categorical True --transform_label_log False --csv_path data/gamma.csv --label_col label"
+
+        Write-Host "Executing: $command" -ForegroundColor Green
+
+        Invoke-Expression $command
+    }
+}
+
+# 각 t 값에 대하여 반복
+foreach ($t in $t_values) {
+    # 각 eps 값에 대하여 반복
+    foreach ($eps in $eps_values) {
+        $command = "python pm_transform_train_test_batch_label.py --t $t --eps $eps --transform_label_numerical False --transform_label_categorical True --transform_label_log False --csv_path data/credit.csv --label_col label"
 
         Write-Host "Executing: $command" -ForegroundColor Green
 
